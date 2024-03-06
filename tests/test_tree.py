e6ec74b8a3b6c463d9c045600400bc13dcad7d45
tsannie <tsannie@student.42.fr>
2024-03-06 18:11:18 +0100

    add test and update python version for poetry

---
+++ tests/test_tree.py
@@ -0,0 +1,22 @@
+# %%
+from foundation import *
+
+
+class Cube:
+    def __init__(self, size=20):
+        self.size = size
+
+    def get_part(self):
+        cube = start_component()
+        # Create a Sketch from XY Plane
+        s = Sketch(cube.xy())
+        square = Square.from_center_and_side(s.origin, self.size)
+        e = Extrusion(square, self.size)
+        cube.add_operation(e)
+        cube.paint("blue")
+        return cube
+
+
+Cube().get_part().to_dict(serializable_nodes=serializable_nodes)
+
+# %%
