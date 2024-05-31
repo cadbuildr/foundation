9dd9c455b5b6f545cff746d509cc09552f2ea23f
Clement Jambou <clement.jambou@coactum.ch>
2024-05-31 17:25:30 +0200

    progress

--- src/foundation/sketch/draw.py
+++ src/foundation/sketch/draw.py
@@ -1,8 +1,13 @@
 from foundation.sketch.point import Point
 from foundation.sketch.primitives.line import Line
-from foundation.sketch.closed_sketch_shape import Polygon
+from foundation.sketch.primitives.arc import Arc
+from foundation.sketch.closed_sketch_shape import (
+    Polygon,
+    CustomClosedSketchShape,
+    SketchPrimitiveTypes,
+)
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from foundation.sketch.sketch import Sketch
@@ -19,6 +24,7 @@ class Draw:
         self.point_added = False
         self.point_idx = 0
         self.points: list[Point] = []
+        self.primitives: list[SketchPrimitiveTypes] = []
 
     def move_to(self, x: float, y: float):
         """Move to absolute position"""
@@ -37,7 +43,7 @@ class Draw:
             point = self.sketch.origin
         else:
             point = Point(self.sketch, self.x, self.y)
-        self.points.append(Point(self.sketch, self.x, self.y))
+        self.points.append(point)
         self.point_idx = len(self.points) - 1
 
     def line_to(self, x: float, y: float):
@@ -47,7 +53,20 @@ class Draw:
         self.y = y
         self.add_point()
         self.point_added = True
+        self.primitives.append(Line(self.points[-2], self.points[-1]))
         return
+
+    def arc_to(self, x: float, y: float, radius: float):
+        # TODO
+        if not self.point_added:
+            self.add_point()
+        self.x = x
+        self.y = y
+        self.add_point()
+        self.point_added = True
+        self.primitives.append(
+            Arc.from_two_points_and_radius(self.points[-2], self.points[-1], radius)
+        )
 
     def line(self, dx: float, dy: float):
         if not self.point_added:
@@ -56,6 +75,7 @@ class Draw:
         self.y += dy
         self.add_point()
         self.point_added = True
+        self.primitives.append(Line(self.points[-2], self.points[-1]))
         return
 
     def back_one_point(self):
@@ -64,9 +84,26 @@ class Draw:
             self.point_idx = 0
 
     def get_closed_polygon(self) -> Polygon:
-        lines = []
-        for i in range(len(self.points) - 1):
-            lines.append(Line(self.points[i], self.points[i + 1]))
+        # Deprecated use get_closed_shape.
+        lines: List[Line] = []
+
+        # Check that all the primitives are lines and filter them into a new list
+        for primitive in self.primitives:
+            if not isinstance(primitive, Line):
+                raise ValueError("All primitives must be lines")
+            lines.append(primitive)
+
+        # for i in range(len(self.points) - 1):
+        #     lines.append(Line(self.points[i], self.points[i + 1]))
         if self.points[0] != self.points[-1]:
             lines.append(Line(self.points[-1], self.points[0]))
+        print(lines)
         return Polygon(self.sketch, lines)
+
+    def get_closed_shape(self) -> CustomClosedSketchShape:
+        primitives = []
+        for primitive in self.primitives:
+            primitives.append(primitive)
+        if self.points[0] != self.points[-1]:
+            primitives.append(Line(self.points[-1], self.points[0]))
+        return CustomClosedSketchShape(self.sketch, primitives)
--- tests/sketch/primitives/test_arc.py
+++ tests/sketch/primitives/test_arc.py
@@ -10,7 +10,7 @@ class TestArc(unittest.TestCase):
         self.sketch = Sketch(comp.xy())
 
     def test_from_two_points_and_radius_valid_example(self):
-        # Points (-1,0) and (1,0) with radius 2
+        # Points (-1,0) and (1,0) with radius 1
         p1 = Point(self.sketch, -1, 0)
         p2 = Point(self.sketch, 1, 0)
         radius = 1
@@ -33,7 +33,7 @@ class TestArc(unittest.TestCase):
         self.assertEqual(arc.sketch, self.sketch)
 
     def test_from_two_points_and_radius_valid_example_reversed(self):
-        # Points (1,0) and (-1,0) with radius 2 (reversed order)
+        # Points (1,0) and (-1,0) with radius 1 (reversed order)
         p1 = Point(self.sketch, 1, 0)
         p2 = Point(self.sketch, -1, 0)
         radius = 1
@@ -46,6 +46,26 @@ class TestArc(unittest.TestCase):
         # Assert the points are correctly set
         self.assertEqual(arc.p1, p1)
 
+        self.assertEqual(arc.p3, p2)
+        self.assertTrue(isclose(arc.p2.x.value, expected_p2_x))
+        self.assertTrue(isclose(arc.p2.y.value, expected_p2_y))
+
+        # Assert the sketch is correctly associated
+        self.assertEqual(arc.sketch, self.sketch)
+
+    def test_two_points_with_negative_radius(self):
+        # Points (-1,0) and (1,0) with radius -1
+        p1 = Point(self.sketch, -1, 0)
+        p2 = Point(self.sketch, 1, 0)
+        radius = -1
+        arc = Arc.from_two_points_and_radius(p1, p2, radius)
+
+        # Calculate the expected p3 coordinates
+        expected_p2_x = 0
+        expected_p2_y = -1
+
+        # Assert the points are correctly set
+        self.assertEqual(arc.p1, p1)
         self.assertEqual(arc.p3, p2)
         self.assertTrue(isclose(arc.p2.x.value, expected_p2_x))
         self.assertTrue(isclose(arc.p2.y.value, expected_p2_y))
